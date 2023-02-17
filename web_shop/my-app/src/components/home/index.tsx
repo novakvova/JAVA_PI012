e8c3fb95352fa57ad1e45ca62f028af9450518c7
novakvova <novakvova@gmail>
2023-02-17 11:34:37 +0200

    Begin working get request

--- web_shop/my-app/src/components/home/index.tsx
+++ web_shop/my-app/src/components/home/index.tsx
@@ -1,4 +1,15 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 const Home = () => {
+
+    useEffect(() => {
+         axios.get("http://localhost:8081/").then(resp => {
+            console.log("resp = ", resp);
+         });
+      
+    }, [])
+    
     return (
         <>
             <h1 className='text-3xl font-bold underline'>Hello React!</h1>
