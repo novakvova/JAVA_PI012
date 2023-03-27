ebd4e025d6b003c6fe08e8c1cc0ce4ef711914a5
novakvova <novakvova@gmail>
2023-03-27 10:28:35 +0300

    Add Formik

--- web_shop/my-app/src/components/auth/login/LoginPage.tsx
+++ web_shop/my-app/src/components/auth/login/LoginPage.tsx
@@ -1,10 +1,35 @@
 import { FcGoogle } from "react-icons/fc";
-import { AiFillFacebook } from "react-icons/ai";
 
 import loginImg from "../../../assets/login.jpg";
 import { Link } from "react-router-dom";
+import { ILoginPage } from "../types";
+import * as yup from "yup";
+import { useFormik } from "formik";
+
 
 const LoginPage = () => {
+  const initValues : ILoginPage ={
+    email: "",
+    password: ""
+  };
+
+  const loginSchema = yup.object({
+    email: yup.string().required("Поле не повинне бути пустим"),
+    password: yup.string().required("Поле не повинне бути пустим"),
+  });
+
+  const onSubmitFormik =(values: ILoginPage) => {
+    console.log("Login form: ", values);
+  }
+
+  const formik = useFormik({
+    initialValues: initValues,
+    onSubmit: onSubmitFormik,
+    validationSchema: loginSchema
+  });
+
+  const {values, errors, touched, handleSubmit, handleChange, setFieldValue} = formik;
+
   return (
     <>
       <div className="relative w-full h-screen bg-zinc-900/90">
@@ -15,26 +40,41 @@ const LoginPage = () => {
         />
 
         <div className="flex justify-center py-10 ">
-          <form className="max-w-[400px] w-full mx-auto bg-white p-8">
+          <form className="max-w-[400px] w-full mx-auto bg-white p-8" onSubmit={handleSubmit}>
             <h2 className="text-4xl font-bold text-center py-2">Вхід на сайт</h2>
-            <div className="flex justify-between py-8">
-              <p className="border shadow-lg hover:shadow-xl px-6 py-2 relative flex items-center">
-                <AiFillFacebook className="mr-2" /> Facebook
-              </p>
-              <p className="border shadow-lg hover:shadow-xl px-6 py-2 relative flex items-center">
-                <FcGoogle className="mr-2" /> Google
-              </p>
-            </div>
+           
             <div className="flex flex-col mb-4">
               <label>Username</label>
-              <input className="border relative bg-gray-100 p-2" type="text" />
+              <input className="border relative bg-gray-100 p-2" 
+                type="text"
+                name="email"
+                id="email"
+                onChange={handleChange}
+                value={values.email}
+                />
+              {errors.email && 
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                <span className="font-medium">{errors.email}</span> 
+                
+              </p>  
+              }
             </div>
             <div className="flex flex-col ">
               <label>Password</label>
               <input
                 className="border relative bg-gray-100 p-2"
                 type="password"
+                name="password"
+                id="password"
+                onChange={handleChange}
+                value={values.password}
               />
+              {errors.password && 
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                <span className="font-medium">{errors.password}</span> 
+                
+              </p>
+              }
             </div>
             <button className="w-full py-3 mt-8 bg-indigo-600 hover:bg-indigo-500 relative text-white">
               Sign In
@@ -47,6 +87,14 @@ const LoginPage = () => {
               <p className="text-center mt-8">Not a member? Sign up now</p>
             </Link>
             
+            <div className="flex justify-between py-8">
+              {/* <p className="border shadow-lg hover:shadow-xl px-6 py-2 relative flex items-center">
+                <AiFillFacebook className="mr-2" /> Facebook
+              </p> */}
+              <p className="border shadow-lg hover:shadow-xl px-6 py-2 relative flex items-center">
+                <FcGoogle className="mr-2" /> Google
+              </p>
+            </div>
           </form>
         </div>
       </div>
